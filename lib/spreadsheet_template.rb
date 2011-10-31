7ed2a7cd2a37076a7f6755e98a8631df4c600210
Craig Knox <cknox@insiliflo.com>
2011-10-31 16:30:52 -0600

    Require spreadsheet so explicit requirement on spreadsheet isn't required in project Gemfile.

--- lib/spreadsheet_template.rb
+++ lib/spreadsheet_template.rb
@@ -1,5 +1,6 @@
 # encoding: utf-8
 require 'action_view'
+require 'spreadsheet'
 require 'spreadsheet_template/template_handler'
 require 'spreadsheet_template/railtie'
 require 'spreadsheet_template/version'
